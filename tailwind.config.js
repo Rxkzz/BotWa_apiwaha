4a7d0648a4eadb32efb5a3d22adfccff050b2d3d
Rxkzz <rizkialhabsi6@gmail.com>
2024-07-16 13:27:22 +0700

    styling send message

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}', // Menentukan file mana yang harus di-scan untuk kelas Tailwind
+  ],
+  theme: {
+    extend: {}, // Menambahkan tema tambahan jika diperlukan
+  },
+  plugins: [], // Menambahkan plugin tambahan jika diperlukan
+};
